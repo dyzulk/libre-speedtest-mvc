d6a0341da57193b1419406e9ded4ea08b63625da
dyzulk <66510723+dyzulk@users.noreply.github.com>
2026-08-05 13:31:17 +0700

    fix: disable reverse proxy buffering using X-Accel-Buffering header

--- app/Traits/RequestHelper.php
+++ app/Traits/RequestHelper.php
@@ -78,5 +78,6 @@ trait RequestHelper
         header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0, s-maxage=0');
         header('Cache-Control: post-check=0, pre-check=0', false);
         header('Pragma: no-cache');
+        header('X-Accel-Buffering: no');
     }
 }
